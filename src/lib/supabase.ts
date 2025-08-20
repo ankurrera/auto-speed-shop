88743c779b2fbcb917cdce28e132614cbead4638
ankur <ankurr.era@gmail.com>
2025-08-20 16:27:10 +0530

    m

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../database.types';
 
+// Access the variables via process.env
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-// Add these console.log lines
-console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Anon Key:", supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and Key are required in environment variables');
