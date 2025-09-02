3b85d2abfa9fbdf38a7321b48f5bbffccf1b0546
ankur <ankurr.era@gmail.com>
2025-09-02 13:13:28 +0530

    m

--- src/App.tsx
+++ src/App.tsx
@@ -13,49 +13,58 @@ import Cart from "./pages/Cart";
 import Wishlist from "./pages/Wishlist";
 import Account from "./pages/Account";
 import ResetPassword from "./pages/ResetPassword";
-import Checkout from "./pages/Checkout"; // Import the new Checkout page
+import Checkout from "./pages/Checkout";
 import NotFound from "./pages/NotFound";
 import ProductDetails from "./pages/ProductDetails";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { CartProvider } from "./contexts/CartContext";
 import { WishlistProvider } from "./contexts/WishlistContext";
 import ScrollToTop from "./components/ScrollToTop";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const queryClient = new QueryClient();
+
+// This is your sandbox client ID. You will need to replace this with your actual client ID.
+const paypalOptions = {
+  "clientId": "AfxRHwPiPRPrde7vSZQRLx2IQxkSzfFmH-5-dFeD91Zs1F7ceQsPkwgXIqWBRyZoz4PMYD_GigzU58HU",
+  "currency": "USD",
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <ScrollToTop />
-          <CartProvider>
-            <WishlistProvider>
-              <div className="min-h-screen flex flex-col">
-                <Header />
-                <main className="flex-1">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/shop" element={<Shop />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/wishlist" element={<Wishlist />} />
-                    <Route path="/account" element={<Account />} />
-                    <Route path="/account/reset-password" element={<ResetPassword />} />
-                    <Route path="/checkout" element={<Checkout />} /> {/* Add the new route for the checkout page */}
-                    <Route path="/products/:id" element={<ProductDetails />} />
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
-                </main>
-                <Footer />
-              </div>
-            </WishlistProvider>
-          </CartProvider>
-        </BrowserRouter>
-      </TooltipProvider>
+      <PayPalScriptProvider options={paypalOptions}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <ScrollToTop />
+            <CartProvider>
+              <WishlistProvider>
+                <div className="min-h-screen flex flex-col">
+                  <Header />
+                  <main className="flex-1">
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/shop" element={<Shop />} />
+                      <Route path="/about" element={<About />} />
+                      <Route path="/contact" element={<Contact />} />
+                      <Route path="/cart" element={<Cart />} />
+                      <Route path="/wishlist" element={<Wishlist />} />
+                      <Route path="/account" element={<Account />} />
+                      <Route path="/account/reset-password" element={<ResetPassword />} />
+                      <Route path="/checkout" element={<Checkout />} />
+                      <Route path="/products/:id" element={<ProductDetails />} />
+                      <Route path="*" element={<NotFound />} />
+                    </Routes>
+                  </main>
+                  <Footer />
+                </div>
+              </WishlistProvider>
+            </CartProvider>
+          </BrowserRouter>
+        </TooltipProvider>
+      </PayPalScriptProvider>
     </ThemeProvider>
   </QueryClientProvider>
 );
